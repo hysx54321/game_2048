7d09b4105cde93b519ef7c90ebcadc03d756bf67
Yixiao Shen <shen0138@e.ntu.edu.sg>
2020-01-23 16:23:36 +0800

    removed files

--- two_zero_four_eight/two_zero_four_eight/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TwoZeroFourEightConfig(AppConfig):
-    name = 'two_zero_four_eight'
--- two_zero_four_eight/two_zero_four_eight/helper_classes/Message.py
+++
@@ -1,4 +0,0 @@
-class Message:
-    def __init__(self, content, level):
-        self.content = content
-        self.level = level
